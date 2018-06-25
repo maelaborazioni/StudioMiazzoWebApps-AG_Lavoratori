fc92109570195d9674fd6d29135bcdbea10fe41a
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2018-06-25 14:58:42 +0200

    [ADD] Calculation iddittaclassificazione

---
+++ datasources/ma_anagrafiche/lavoratori_classificazioni_calculations.js
@@ -0,0 +1,7 @@
+/**
+ * @properties={type:8,typeid:36,uuid:"1C46A1F8-4C5D-4FD7-91C0-FC17D1DDCADB"}
+ */
+function iddittaclassificazione()
+{
+	return lavoratori_classificazioni_to_ditte_classificazioni.iddittaclassificazione;
+}
