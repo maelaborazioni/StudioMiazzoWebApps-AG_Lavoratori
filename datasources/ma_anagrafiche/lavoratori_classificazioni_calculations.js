8108cc390c7d0eee050e0de5794ee465fb741010
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2018-08-10 09:30:19 +0200

    [ADD] Calculation iddittariferimento

--- datasources/ma_anagrafiche/lavoratori_classificazioni_calculations.js
+++ datasources/ma_anagrafiche/lavoratori_classificazioni_calculations.js
@@ -1,4 +1,12 @@
 /**
+ * @properties={type:12,typeid:36,uuid:"DF624ADF-A63A-4C86-B859-6C1D17F923A3"}
+ */
+function iddittariferimento()
+{
+	return globals.getDittaRiferimento(idditta) || idditta;
+}
+
+/**
  * @properties={type:8,typeid:36,uuid:"1C46A1F8-4C5D-4FD7-91C0-FC17D1DDCADB"}
  */
 function iddittaclassificazione()
