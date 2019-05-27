ec0256ceb0d1c59d879f9e90c20c265001c50035
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2019-05-27 11:42:32 +0200

    [ADD] Web service per ottenere informazioni anagrafiche di un lavoratore da applicazioni di terze parti

---
+++ forms/agl_ws.frm
@@ -0,0 +1,7 @@
+customProperties:"formComponent:false",
+encapsulation:60,
+name:"agl_ws",
+showInMenu:true,
+styleName:"leaf_style",
+typeid:3,
+uuid:"DB3D8B5B-612C-48ED-8E98-9E55318BE290"
---
+++ forms/agl_ws.js
@@ -0,0 +1,17 @@
+/**
+ * @properties={typeid:24,uuid:"967A9B8F-04AB-44AD-A635-891E39D09332"}
+ */
+function ws_create()
+{
+	var args = arguments[0];
+	
+	var id = args['lavoratore'];
+	var db = args['cliente'];
+	
+	var sql = "SELECT * FROM Lavoratori WHERE idlavoratore = ?"
+	var ds = databaseManager.getDataSetByQuery('Cliente_' + db,
+				                              sql,
+											  [id],
+											  1);
+	return JSON.stringify(ds);
+}
